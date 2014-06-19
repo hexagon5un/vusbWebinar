efba292e7c2cb5245392e320f3867af9cb0945a2
Elliot Williams <elliot@pre.postero.us>
2014-06-19 14:24:27 +0200

    changed command, test runner works

--- project_weatherThing/firmware/main.c
+++ project_weatherThing/firmware/main.c
@@ -1,23 +1,3 @@
-/* Name: main.c
- * Project: custom-class, a basic USB example
- * Author: Christian Starkjohann
- * Creation Date: 2008-04-09
- * Tabsize: 4
- * Copyright: (c) 2008 by OBJECTIVE DEVELOPMENT Software GmbH
- * License: GNU GPL v2 (see License.txt), GNU GPL v3 or proprietary (CommercialLicense.txt)
- */
-
-/*
-This example should run on most AVRs with only little changes. No special
-hardware resources except INT0 are used. You may have to change usbconfig.h for
-different I/O pins for USB. Please note that USB D+ must be the INT0 pin, or
-at least be connected to INT0 as well.
-We assume that an LED is connected to port B bit 0. If you connect it to a
-different port or bit, change the macros below:
-*/
-#define LED_PORT_DDR        DDRB
-#define LED_PORT_OUTPUT     PORTB
-#define LED_BIT             0
 
 #include <avr/io.h>
 #include <avr/wdt.h>
@@ -29,6 +9,11 @@ different port or bit, change the macros below:
 #include "oddebug.h"        /* This is also an example for using debug macros */
 #include "requests.h"       /* The custom request numbers we use */
 
+#define LED_PORT_DDR        DDRB
+#define LED_PORT_OUTPUT     PORTB
+#define LED_BIT             0
+
+#define CMD_SET_SERVO       0x42
 /* ------------------------------------------------------------------------- */
 /* ----------------------------- USB interface ----------------------------- */
 /* ------------------------------------------------------------------------- */
@@ -51,6 +36,9 @@ static uchar    dataBuffer[4];  /* buffer must stay valid when usbFunctionSetup 
         }else{                          /* clear LED */
             LED_PORT_OUTPUT &= ~_BV(LED_BIT);
         }
+    }else if(rq->bRequest == CMD_SET_SERVO){
+            LED_PORT_OUTPUT ^= _BV(LED_BIT);
+        return 0;                       /* tell the driver to send 1 byte */
     }else if(rq->bRequest == CUSTOM_RQ_GET_STATUS){
         dataBuffer[0] = ((LED_PORT_OUTPUT & _BV(LED_BIT)) != 0);
         usbMsgPtr = dataBuffer;         /* tell the driver which data to return */
---
+++ project_weatherThing/firmware/test_custom.py
@@ -0,0 +1,18 @@
+import usb
+import time
+
+OUT_TYPE = usb.util.build_request_type(usb.util.CTRL_OUT, usb.util.CTRL_TYPE_VENDOR, usb.util.CTRL_RECIPIENT_DEVICE)
+
+## Command set
+CMD_SET_SERVO = 0x42
+
+dev = usb.core.find(idVendor=0x16c0, idProduct=0x05dc)
+
+## Toggle LED, read status, then echo some data
+for i in range(10):
+    dev.ctrl_transfer(
+            bmRequestType = OUT_TYPE, 
+            bRequest      = CMD_SET_SERVO, 
+            wValue        = 0
+            )
+    time.sleep(0.5)
