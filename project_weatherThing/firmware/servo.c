df7d355baa3f9172f3fdf42ac4deaa2b1600c751
Elliot Williams <elliot@pre.postero.us>
2014-06-19 14:17:05 +0200

    importing in working examle custom
    going to strip it down until it shines

---
+++ project_weatherThing/firmware/servo.c
@@ -0,0 +1,11 @@
+
+#include "servo.h"
+
+void initServo(){
+
+}
+
+void setServo(uint16_t pulse){
+/* Sets the servo to the correct position */ 
+	return 0;
+}
---
+++ project_weatherThing/firmware/servo.h
@@ -0,0 +1,18 @@
+
+/* ------------------------------------------------------------------------- */
+/* ----------------------------- Servo Functions --------------------------- */
+/* ------------------------------------------------------------------------- */
+
+#include <avr/io.h>
+#include <avr/interrupt.h>  /* for sei() */
+
+// For great blinkage
+#define LED_PORT_DDR        DDRB
+#define LED_PORT_OUTPUT     PORTB
+#define LED_BIT             0
+
+void initServo(void);
+/* Sets up timer1 to send servo pulses */
+
+void setServo(uint16_t pulse);
+/* Sets the servo to the specified position */ 
